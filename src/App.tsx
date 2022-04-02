1d1f97159cdf037cc4e3a058cb54ebf9f82680c6
syy11cn <syy11cn@outlook.com>
2022-04-02 17:36:26 +0800

    feat: dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { useRoutes } from 'react-router-dom'
+import Footer from './components/footer'
 import { routes } from './routes'
 
 const App = () => {
   return <>
     <p>hello world</p>
     {useRoutes(routes)}
+    <Footer></Footer>
   </>
 }
 
--- src/utils/dark.tsx
+++ src/utils/dark.tsx
@@ -1,13 +1,14 @@
 export function useDark() {
-  const [value, setValue] = useState<string>(window.localStorage.getItem('colorSchema') || 'auto')
+  const [value, setValue] = useState<string>(window.localStorage.getItem('color-schema') || 'auto')
   const toggleDark = () => {
-    setValue(value === 'auto' ? 'dark' : 'auto')
-    window.localStorage.setItem('colorSchema', value)
+    const newValue = value === 'auto' ? 'dark' : 'auto'
+    setValue(newValue)
+    window.localStorage.setItem('color-schema', newValue)
   }
   const isDark = value === 'dark'
 
   if (document)
     useEffect(() => { document.documentElement.classList.toggle('dark', isDark) })
 
-  return [isDark, toggleDark]
+  return [isDark, toggleDark] as [boolean, () => void]
 }
